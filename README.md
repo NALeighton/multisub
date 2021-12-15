3c50f314ff06f31dd82a16064135bdabacac5554
Alex Leighton <naleighton@gmail.com>
2021-12-15 11:47:26 -0500

    Setup gtk

---
+++ README.md
@@ -0,0 +1,3 @@
+# Multisub
+
+Multisub is an easy to use application that allows you to subscribe to multicast groups. This allows you capture multicast traffic in programs such as Wireshark that you normally can't access due to not being subscribed to the proper multicast group(s).
---
+++ multisub/Main.cs
@@ -0,0 +1,15 @@
+﻿using Gtk;
+
+class Hello
+{
+    static void Main()
+    {
+        Application.Init();
+
+        Window window = new Window("Hello World");
+        window.DeleteEvent += delegate { Application.Quit(); };
+        window.Show();
+        
+        Application.Run();
+    }
+}
