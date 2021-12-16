0de73ec756df787812d6097606a94005c4689448
Alex Leighton <naleighton@gmail.com>
2021-12-16 15:00:13 -0500

    Setup UI

--- multisub/Main.cs
+++ multisub/Main.cs
@@ -1,14 +1,41 @@
 ﻿using Gtk;
 
-class Hello
+namespace multisub;
+
+class Multisub
 {
+    private static Entry? _multicastGroup;
+    private static ToggleButton? _bind;
     static void Main()
     {
         Application.Init();
 
-        Window window = new Window("Hello World");
+        Window window = new Window("Multisub");
         window.DeleteEvent += delegate { Application.Quit(); };
-        window.Show();
+        VBox outerVBox = new VBox();
+        HBox groupHBox = new HBox();
+        groupHBox.Add(new Label("Multicast Group"));
+        _multicastGroup = new Entry();
+        _multicastGroup.MarginStart = 5;
+        groupHBox.Add(_multicastGroup);
+        outerVBox.Add(groupHBox);
+        _bind = new ToggleButton();
+        _bind.Active = false;
+        _bind.Label = "Bind";
+        _bind.Clicked += delegate
+        {
+            if (_bind.Active)
+            {
+                _bind.Label = "Unbind";
+            }
+            else
+            {
+                _bind.Label = "Bind";
+            }
+        };
+        outerVBox.Add(_bind);
+        window.Add(outerVBox);
+        window.ShowAll();
         
         Application.Run();
     }
