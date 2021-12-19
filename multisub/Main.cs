56a8e08af27ee06addf72a33e984b08ab07e9ce4
Alex Leighton <naleighton@gmail.com>
2021-12-19 13:55:45 -0500

    Add network interface selector

--- multisub/Main.cs
+++ multisub/Main.cs
@@ -1,11 +1,16 @@
 ﻿using Gtk;
+using System.Net.NetworkInformation;
 
 namespace multisub;
 
 class Multisub
 {
     private static Entry? _multicastGroup;
+    private static ComboBox? _interfaceCombo;
     private static ToggleButton? _bind;
+
+    private static List<string> _interfaceListStore = new();
+    
     static void Main()
     {
         Application.Init();
@@ -19,6 +24,15 @@ class Multisub
         _multicastGroup.MarginStart = 5;
         groupHBox.Add(_multicastGroup);
         outerVBox.Add(groupHBox);
+        HBox interfaceHBox = new HBox();
+        interfaceHBox.Add(new Label("Interface"));
+        foreach (var networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+        {
+            _interfaceListStore.Add(networkInterface.Name);
+        }
+        _interfaceCombo = new ComboBox(_interfaceListStore.ToArray());
+        interfaceHBox.Add(_interfaceCombo);
+        outerVBox.Add(interfaceHBox);
         _bind = new ToggleButton();
         _bind.Active = false;
         _bind.Label = "Bind";
@@ -28,11 +42,13 @@ class Multisub
             {
                 _bind.Label = "Unbind";
                 _multicastGroup.Sensitive = false;
+                _interfaceCombo.Sensitive = false;
             }
             else
             {
                 _bind.Label = "Bind";
                 _multicastGroup.Sensitive = true;
+                _interfaceCombo.Sensitive = true;
             }
         };
         outerVBox.Add(_bind);
